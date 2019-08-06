0ad0ef1664582a9b499c6469235a3e080e78f7ee
Konstantin Mihailov <kahollavan48@gmail.com>
2019-08-06 11:05:42 +0300

    https

--- src/app/services/city-service.service.ts
+++ src/app/services/city-service.service.ts
@@ -9,18 +9,18 @@ export class CityServiceService {
   constructor(private http: HttpClient) { }
 
   getDefaultLocation(lat, lon) {
-    return this.http.get(`http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=bQ45spNGkGXvHEYnK5C5UTA5oIgTD3VW&q=${lat}%2C${lon}`);
+    return this.http.get(`https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=bQ45spNGkGXvHEYnK5C5UTA5oIgTD3VW&q=${lat}%2C${lon}`);
   }
 
   getCityKeyByCityName(cityName) {
-    return this.http.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=%09bQ45spNGkGXvHEYnK5C5UTA5oIgTD3VW&q=${cityName}`)
+    return this.http.get(`https://dataservice.accuweather.com/locations/v1/cities/search?apikey=%09bQ45spNGkGXvHEYnK5C5UTA5oIgTD3VW&q=${cityName}`)
   }
 
   getWeatherByCityKey(cityKey, flag) {
     if (flag) {
-      return this.http.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=bQ45spNGkGXvHEYnK5C5UTA5oIgTD3VW`);
+      return this.http.get(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=bQ45spNGkGXvHEYnK5C5UTA5oIgTD3VW`);
     }
-    return this.http.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=bQ45spNGkGXvHEYnK5C5UTA5oIgTD3VW&metric=true`);
+    return this.http.get(`https://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=bQ45spNGkGXvHEYnK5C5UTA5oIgTD3VW&metric=true`);
   }
 }
 
